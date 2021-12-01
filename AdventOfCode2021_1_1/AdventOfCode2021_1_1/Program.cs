7659c5969889524167043738a26300982923826c
taydexx <jakub.tadeusz@interia.pl>
2021-12-01 16:01:22 +0100

    AoC day 1

---
+++ AdventOfCode2021_1_1/AdventOfCode2021_1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] lines = File.ReadAllLines("input");
+            int largerMeasurements = 0;
+            for(int i = 1; i < lines.Length; i++)
+            {
+                if (int.Parse(lines[i]) > int.Parse(lines[i - 1])) largerMeasurements++;
+            }
+            Console.WriteLine(largerMeasurements);
+        }
+    }
+}
---
+++ AdventOfCode2021_1_2/AdventOfCode2021_1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2021_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = new List<int>();
+            StreamReader dataStream = new StreamReader("input");
+            string datasample;
+            while ((datasample = dataStream.ReadLine()) != null)
+            {
+
+                // datasample has the current line of text - write it to the console.
+                lines.Add(int.Parse(datasample));
+            }
+
+            int largerMeasurements = 0;
+            int prevSum = lines[0] + lines[1] + lines[2];
+            for (int i = 1; i < lines.Count-2; i++)
+            {
+                int newSum = lines[i] + lines[i + 1] + lines[i + 2];
+                if (newSum > prevSum) largerMeasurements++;
+                prevSum = newSum;
+            }
+            Console.WriteLine(largerMeasurements);
+        }
+    }
+}
