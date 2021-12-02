b0b08d7bef717731246a5eb7415dc8b3063292cf
taydexx <jakub.tadeusz@interia.pl>
2021-12-02 19:13:21 +0100

    Advent of Code day two

---
+++ AdventOfCode2021_2_1/AdventOfCode2021_2_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horizontal = 0;
+            int depth = 0;
+            StreamReader dataStream = new StreamReader("input");
+            string datasample;
+            while ((datasample = dataStream.ReadLine()) != null)
+            {
+                //parse data from input file
+                string command = datasample.Split(' ')[0];
+                int data = int.Parse(datasample.Split(' ')[1]);
+                switch (command)
+                {
+                    case "forward": horizontal += data; break;
+                    case "down": depth += data; break;
+                    case "up": depth -= data; break;
+                }
+            }
+            Console.WriteLine(horizontal * depth);
+
+        }
+    }
+}
---
+++ AdventOfCode2021_2_2/AdventOfCode2021_2_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+            StreamReader dataStream = new StreamReader("input");
+            string datasample;
+            while ((datasample = dataStream.ReadLine()) != null)
+            {
+                //parse data from input file
+                string command = datasample.Split(' ')[0];
+                int data = int.Parse(datasample.Split(' ')[1]);
+                switch (command)
+                {
+                    case "down": aim+= data; break;
+                    case "up": aim -= data; break;
+                    case "forward": 
+                        horizontal += data;
+                        depth += data * aim;
+                        break;
+                }
+            }
+            Console.WriteLine(horizontal * depth);
+        }
+    }
+}
