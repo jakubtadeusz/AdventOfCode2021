a30c279b53e2a54c0308db6371667c030cfef228
taydexx <jakub.tadeusz@interia.pl>
2021-12-03 17:51:02 +0100

    add day 3 solutions

---
+++ AdventOfCode2021_3_1/AdventOfCode2021_3_1/Program.cs
@@ -0,0 +1,49 @@
+﻿ using System;
+
+namespace AdventOfCode2021_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input");
+            int length = lines[0].Length;
+            int[] gamma = new int[length];
+            int[] epsilon = new int[length];
+            for(int i = 0; i < length; i++)
+            {
+                int zeros = 0;
+                int ones = 0;
+                for (int j = 0; j < lines.Length; j++)
+                {
+                    switch (lines[j].ToCharArray()[i])
+                    {
+                        case '0': zeros++; break;
+                        case '1': ones++; break;
+                    }
+                }
+                if (zeros > ones)
+                {
+                    gamma[i] = 0;
+                    epsilon[i] = 1;
+                }
+                else
+                {
+                    gamma[i] = 1;
+                    epsilon[i] = 0;
+                }
+            }
+
+            int gammaValue = 0;
+            int epsilonValue = 0;
+            int x = 1;
+            for(int i = length-1; i >= 0; i--)
+            {
+                gammaValue += x * gamma[i];
+                epsilonValue += x * epsilon[i];
+                x *= 2;
+            }
+            Console.WriteLine(gammaValue*epsilonValue);
+        }
+    }
+}
---
+++ AdventOfCode2021_3_2/AdventOfCode2021_3_2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2021_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input");
+            int length = lines[0].Length;
+            List<string> linesList = new List<string>(lines);
+            int i = 0;
+            while (linesList.Count != 1)
+            {
+                int ones = 0;
+                int zeros = 0;
+                for(int j = 0; j < linesList.Count; j++)
+                {
+                    if(linesList[j].ToCharArray()[i] == '1')
+                    {
+                        ones++;
+                    }
+                    else zeros++;
+                }
+                if(ones >= zeros)
+                {
+                    linesList.RemoveAll(x => x.ToCharArray()[i] == '0');
+                }else linesList.RemoveAll(x => x.ToCharArray()[i] == '1');
+                i++;
+            }
+            string oxygenRating = linesList[0];
+
+            linesList = new List<string>(lines);
+            i = 0;
+            while (linesList.Count != 1)
+            {
+                int ones = 0;
+                int zeros = 0;
+                for (int j = 0; j < linesList.Count; j++)
+                {
+                    if (linesList[j].ToCharArray()[i] == '1')
+                    {
+                        ones++;
+                    }
+                    else zeros++;
+                }
+                if (ones < zeros)
+                {
+                    linesList.RemoveAll(x => x.ToCharArray()[i] == '0');
+                }
+                else linesList.RemoveAll(x => x.ToCharArray()[i] == '1');
+                i++;
+            }
+
+            string co2ScrubberRating = linesList[0];
+
+            int oxygenValue = 0;
+            int co2ScrubberValue = 0;
+            int x = 1;
+            for (i = length - 1; i >= 0; i--)
+            {
+                oxygenValue += x * int.Parse(oxygenRating.ToCharArray()[i].ToString());
+                co2ScrubberValue += x * int.Parse(co2ScrubberRating.ToCharArray()[i].ToString());
+                x *= 2;
+            }
+            Console.WriteLine(oxygenValue * co2ScrubberValue);
+
+        }
+    }
+}
