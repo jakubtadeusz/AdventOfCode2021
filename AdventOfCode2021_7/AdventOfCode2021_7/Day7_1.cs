d59c5438060a2c2428c2de13b1d6116458996e06
taydexx <jakub.tadeusz@interia.pl>
2021-12-07 19:14:52 +0100

    Add day 7 solutions

---
+++ AdventOfCode2021_7/AdventOfCode2021_7/Day7_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021_7
+{
+    class Day7_1
+    {
+        public static void Run()
+        {
+            int[] lines = Array.ConvertAll(System.IO.File.ReadAllLines("input")[0].Split(','), l => int.Parse(l));
+            Array.Sort(lines);
+            int median = 0;
+            if (lines.Length % 2 == 0)
+            {
+                var firstValue = lines[(lines.Length / 2) - 1];
+                var secondValue = lines[(lines.Length / 2)];
+                median = (int)((firstValue + secondValue) / 2.0);
+            }
+            if (lines.Length % 2 == 1)
+            {
+                median = lines[(lines.Length / 2)];
+            }
+
+            int fuelCost = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                fuelCost += Math.Abs(lines[i] - median);
+            }
+            Console.WriteLine(fuelCost);
+        }
+    }
+}
---
+++ AdventOfCode2021_7/AdventOfCode2021_7/Day7_2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021_7
+{
+    class Day7_2
+    {
+        public static void Run()
+        {
+            int[] lines = Array.ConvertAll(System.IO.File.ReadAllLines("input")[0].Split(','), l => int.Parse(l));
+            int mean = (int)Math.Round(lines.Average());
+            int solution = mean;
+            int solutionCost = CalculateSolutionCost(lines, solution);
+            int newSolutionRight = solution + 1;
+            int newSolutionLeft = solution - 1;
+            int rightSolutionCost = CalculateSolutionCost(lines, newSolutionRight);
+            int leftSolutionCost = CalculateSolutionCost(lines, newSolutionLeft);
+            while(rightSolutionCost < solutionCost)
+            {
+                solution = newSolutionRight;
+                newSolutionRight = solution + 1;
+                solutionCost = rightSolutionCost;
+            }
+
+            while (leftSolutionCost < solutionCost)
+            {
+                solution = newSolutionLeft;
+                newSolutionLeft = solution + 1;
+                solutionCost = leftSolutionCost;
+            }
+
+            Console.WriteLine(solutionCost);
+        }
+
+        private static int CalculateSolutionCost(int[] lines, int solution)
+        {
+            int cost = 0;
+            for(int i = 0; i < lines.Length; i++)
+            {
+                cost  += CalculateFuelConsumption(lines[i], solution);
+            }
+            return cost;
+        }
+
+        public static int CalculateFuelConsumption(int start, int end)
+        {
+            int cost = 1;
+            int fuelConsumption = 0;
+            if(end < start)
+            {
+                int temp = start;
+                start = end;
+                end = temp;
+            }
+            for(int i = start; i <end; i++)
+            {
+                fuelConsumption += cost;
+                cost++;
+            }
+            return fuelConsumption;
+        }
+    }
+}
