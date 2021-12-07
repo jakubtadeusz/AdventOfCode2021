e0f959be0a9085f3ff1032579112d8f367c1baf6
taydexx <jakub.tadeusz@interia.pl>
2021-12-07 19:14:40 +0100

    Day 6 solutions

---
+++ AdventOfCode2021_6/AdventOfCode2021_6/Day6_1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021_6
+{
+    class Day6_1
+    {
+        public static void Run()
+        {
+            const int DAYS_LENGTH = 80;
+            string lanternfishesInput = System.IO.File.ReadAllLines("input")[0];
+            List<int> lanternfishes = new List<int>(Array.ConvertAll(lanternfishesInput.Split(','), l=>int.Parse(l)));
+            for(int i = 0; i < DAYS_LENGTH; i++)
+            {
+                List<int> newLanternfishes = new List<int>();
+                for(int j = 0; j < lanternfishes.Count; j++)
+                {
+                    if(lanternfishes[j] == 0)
+                    {
+                        lanternfishes[j] = 6;
+                        newLanternfishes.Add(8);
+                    }
+                    else
+                    {
+                        lanternfishes[j] = lanternfishes[j] - 1;
+                    }
+                }
+                lanternfishes.AddRange(newLanternfishes);
+            }
+            Console.WriteLine(lanternfishes.Count);
+        }
+    }
+}
---
+++ AdventOfCode2021_6/AdventOfCode2021_6/Day6_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021_6
+{
+    class Day6_2
+    {
+        public static void Run()
+        {
+            const int DAYS_LENGTH = 256;
+            string lanternfishesInput = System.IO.File.ReadAllLines("input")[0];
+            long [] startingLanternfishes = Array.ConvertAll(lanternfishesInput.Split(','), l => long.Parse(l));
+            long lanternfishesAmount = startingLanternfishes.Length;
+            long[] lanternfishesCounter = new long[9];
+
+            for(int i = 0; i < lanternfishesAmount; i++)
+            {
+                lanternfishesCounter[startingLanternfishes[i]]++;
+            }
+
+            for(int i = 0; i < DAYS_LENGTH; i++)
+            {
+                long[] newLanternfishesCounter = new long[9];
+                lanternfishesAmount += lanternfishesCounter[0];
+                newLanternfishesCounter[8] = lanternfishesCounter[0];
+                newLanternfishesCounter[6] += lanternfishesCounter[0];
+                for(int j = 1; j <= 8; j++)
+                {
+                    newLanternfishesCounter[j - 1] += lanternfishesCounter[j];
+                }
+                lanternfishesCounter = newLanternfishesCounter;
+            }
+
+            Console.WriteLine(lanternfishesAmount);
+        }
+    }
+}
