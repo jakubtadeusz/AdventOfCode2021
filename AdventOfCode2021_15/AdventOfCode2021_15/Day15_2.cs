e45851d1184e10770936196973926fc38c45c65e
taydexx <jakub.tadeusz@interia.pl>
2021-12-15 22:47:25 +0100

    Add day 15 solution

---
+++ AdventOfCode2021_15/AdventOfCode2021_15/Day15_2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2021_15
+{
+    internal class Day15_2
+    {
+        internal static void Run()
+        {
+            string[] lines = System.IO.File.ReadAllLines("input");
+            int[][] map = new int[lines.Length][];
+            int[][] d = new int[map.Length * 5][];
+            //    int[][] prev = new int[map.Length][];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                map[i] = new int[lines[i].Length];
+                //   prev[i] = new int[lines[i].Length];
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    map[i][j] = int.Parse(lines[i][j].ToString());
+                }
+            }
+
+
+            for (int i = 0; i < map.Length * 5; i++)
+            {
+                d[i] = new int[map.Length * 5];
+                for (int j = 0; j < map.Length * 5; j++)
+                {
+                    d[i][j] = int.MaxValue;
+                    //         prev[i][j] = -1;
+                }
+            }
+            d[0][0] = 0;
+
+
+            List<(int, int)> Q = new List<(int, int)>();
+            for (int i = 0; i < map.Length * 5; i++)
+            {
+                for (int j = 0; j < map.Length * 5; j++)
+                {
+                    Q.Add((i, j));
+                }
+            }
+
+            while (Q.Count > 0)
+            {
+
+                (int, int) u = FindMin(Q, d);
+
+                List<(int, int)> neighbors = GetNeighbors(u.Item1, u.Item2, map.Length * 5);
+                foreach (var neighbor in neighbors)
+                {
+                    (int, int) mapCoords = (neighbor.Item1 % map.Length, neighbor.Item2 % map.Length);
+                    int w = (map[mapCoords.Item1][mapCoords.Item2] + neighbor.Item1 / map.Length + neighbor.Item2 / map.Length);
+                    if (w > 9) w -= 9;
+                    if (d[neighbor.Item1][neighbor.Item2] > d[u.Item1][u.Item2] + w)
+                    {
+
+                        d[neighbor.Item1][neighbor.Item2] = d[u.Item1][u.Item2] + w;
+                    }
+                }
+            }
+            Console.WriteLine(d[d.Length - 1][d.Length - 1]);
+        }
+
+        private static (int, int) FindMin(List<(int, int)> Q, int[][] d)
+        {
+            int min = int.MaxValue;
+            (int, int) u = (-1, -1);
+            foreach (var item in Q)
+            {
+                if (d[item.Item1][item.Item2] < min)
+                {
+                    u = item;
+                    min = d[item.Item1][item.Item2];
+                }
+            }
+            Q.Remove(u);
+            return u;
+        }
+
+        private static List<(int, int)> GetNeighbors(int i, int j, int length)
+        {
+            List<(int, int)> neighbors = new List<(int, int)>();
+            if (i > 0) neighbors.Add((i - 1, j));
+            if (i < length - 1) neighbors.Add((i + 1, j));
+            if (j > 0) neighbors.Add((i, j - 1));
+            if (j < length - 1) neighbors.Add((i, j + 1));
+            return neighbors;
+        }
+    }
+}
---
+++ AdventOfCode2021_15/AdventOfCode2021_15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day15_1.Run();
+            Day15_2.Run();
+        }
+    }
+}
